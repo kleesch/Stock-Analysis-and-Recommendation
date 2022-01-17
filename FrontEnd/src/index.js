7a8303f18924a8e700429854e3763ebdb23c57b3
Kyle Leesch <56949068+mrzakch@users.noreply.github.com>
2022-01-16 21:47:37 -0500

    React Application Setup

---
+++ FrontEnd/src/index.js
@@ -0,0 +1,9 @@
+﻿import ReactDOM from 'react-dom';
+import React from 'react';
+
+const info = (<h1>Initialized React App</h1>);
+
+ReactDOM.render(
+    info,
+    document.getElementById('app')
+);
