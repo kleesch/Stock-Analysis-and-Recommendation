0da416e694ada52356aa2b3e92fd39d913c2d13f
idedic98 <84324582+idedic98@users.noreply.github.com>
2022-01-29 15:13:30 -0500

    FinalRestApiFramework

--- SMBackend/backend/views.py
+++ SMBackend/backend/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+# views.py
+from rest_framework import viewsets
+
+from .serializers import PersonSerializer
+from .models import Person
+
+
+class PersonViewSet(viewsets.ModelViewSet):
+    queryset = Person.objects.all().order_by('name')
+    serializer_class = PersonSerializer
