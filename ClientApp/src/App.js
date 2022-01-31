87d4d2159415b3119f8896dcafcb9718e4d80e63
Kyle Leesch <56949068+mrzakch@users.noreply.github.com>
2022-01-30 20:17:48 -0500

    GitIgnore & Cleanup

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,33 +1,26 @@
-﻿import React, { Component } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Layout } from "./components/Layout";
-import { Login } from "./components/login/Login";
+﻿import React, {Component} from "react";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Layout} from "./components/Layout";
+import {Login} from "./components/login/Login";
 
 
 export default class App extends Component {
     static displayName = App.name;
 
     render() {
-        return ( <
-            Layout >
-            <
-            BrowserRouter >
-            <
-            Routes >
-            <
-            Route path = "/login"
-            element = { < Login / > }
-            /> <
-            Route path = "/*"
-            element = { <
-                div className = { `text-light` } >
-                There 's nothing here! <
-                /div>
-            }
-            /> <
-            /Routes> <
-            /BrowserRouter> <
-            /Layout>
+        return (
+            <Layout>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/login" element={< Login/>}/>
+                        <Route path="/*" element={
+                            <div className={`text-light`}>
+                                There's nothing here!
+                            </div>
+                        }/>
+                    </Routes>
+                </BrowserRouter>
+            </Layout>
         );
     }
 }
