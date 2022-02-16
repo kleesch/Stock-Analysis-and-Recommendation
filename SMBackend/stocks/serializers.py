9ce929b2935ca00da1e6b08f25a576e2479e4d51
Kyle Leesch <56949068+mrzakch@users.noreply.github.com>
2022-02-15 23:34:19 -0500

    Basic Stock Registration

---
+++ SMBackend/stocks/serializers.py
@@ -0,0 +1,15 @@
+﻿from rest_framework import serializers
+
+from .models import Stock
+
+
+class StockSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Stock
+        fields = ('ticker', 'company_info')
+
+
+class DailyStockDataSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Stock
+        fields = ("ticker", "open", "high", "low", "close", "volume")
