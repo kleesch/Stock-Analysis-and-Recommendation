8017c586e9a716ccb5dfb40f058cf8b01247e346
Alexjakk <71051562+Alexjakk@users.noreply.github.com>
2022-03-13 20:12:29 -0400

    Feature/combined (#15)
    * Combined all pages (signup, login, homepage)
    
    * Update homepage.js
    
    * Cleaned up code
    
    * Fixed formatting

---
+++ ClientApp/src/components/homepage/homepage.js
@@ -0,0 +1,117 @@
+﻿import React, {Component} from "react";
+import {Button, Card, CardTitle, Input, Alert, Table} from "reactstrap";
+import "./homepage.css";
+
+
+export class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            tickerInput: "",
+            retrievedStock: "None",
+            price: 0,
+            high: 0,
+            low: 0,
+            watchlistStocks: [
+                //{ticker: "IBM", price: 0.00, high: 0.00},
+                {ticker:"None", price:0, high:0},
+                {ticker:"None", price:0, high:0},
+                {ticker:"None", price:0, high:0},
+                {ticker:"None", price:0, high:0},
+                {ticker:"None", price:0, high:0}
+            ]
+        };
+    }
+
+
+    tickerInput(e) {
+        this.setState({
+            tickerInput: e.target.value,
+        })
+    }
+
+    async tickerButton() {
+        const input = this.state.tickerInput
+        const requestOptions = {
+            method: 'GET'
+        }
+        let response = await fetch(`api/dailystocks/getByTicker?ticker=${input}`, requestOptions)
+        if (response.status===200) {
+            response = await response.json();
+            response= await JSON.parse(response);
+            const oldSearch={ticker: input, price: parseFloat(response[response.length-1].fields.close,2), high: parseFloat(response[response.length-1].fields.high,2)};
+            let oldWatchlist=this.state.watchlistStocks;
+            oldWatchlist.splice(0,0,oldSearch);
+            oldWatchlist.splice(3,1);
+            console.log(response[response.length-1]);
+            this.setState({
+                retrievedStock: input,
+                watchlistStocks: oldWatchlist,
+            })
+        }
+    }
+
+    render() {
+        return (
+            <div className="outerContainer4">
+                <div className="innerContainer4">
+                    <div className="loginContainer4">
+                        <Card color={`secondary`} inverse className="loginCard4 innerContainerItem4">
+                            <CardTitle>
+                                <b>Enter stock ticker</b>
+                                <Input className="firstone" placeholder={``} onChange={this.tickerInput.bind(this)}
+                                       value={this.state.tickerInput}> </Input>
+                                <button className="tickerbutton" onClick={this.tickerButton.bind(this)}>Enter</button>                             
+                            </CardTitle>
+                        </Card>
+                        <Card color={`secondary`} inverse className="loginCard4 innerContainerItem4">
+                            <CardTitle>
+                                <b><i>Stock Watchlist</i></b>
+                            </CardTitle>
+                            
+                            <Table className="text-light" hover >
+                                <thead>
+                                    <tr> <th>  Stock Ticker </th>   
+                                         <th>  Price </th>
+                                         <th>  Weekly High </th>
+                                    </tr>
+                                </thead>
+                                    <tbody>
+                                    <tr> <td>  {this.state.watchlistStocks[0].ticker} </td>
+                                         <td>  {this.state.watchlistStocks[0].price} </td>
+                                         <td>  {this.state.watchlistStocks[0].high} </td>
+                                    </tr>
+                                    <tr> <td>  {this.state.watchlistStocks[1].ticker} </td>
+                                         <td>  {this.state.watchlistStocks[1].price} </td>
+                                         <td>  {this.state.watchlistStocks[1].high} </td>
+                                    </tr>
+                                    <tr> <td>  {this.state.watchlistStocks[2].ticker} </td>
+                                         <td>  {this.state.watchlistStocks[2].price} </td>
+                                         <td>  {this.state.watchlistStocks[2].high} </td>
+                                    </tr>
+                                    <tr> <td>  {this.state.watchlistStocks[3].ticker} </td>
+                                         <td>  {this.state.watchlistStocks[3].price} </td>
+                                         <td>  {this.state.watchlistStocks[3].high} </td>
+                                    </tr>
+                                    <tr> <td>  {this.state.watchlistStocks[4].ticker} </td>
+                                         <td>  {this.state.watchlistStocks[4].price} </td>
+                                         <td>  {this.state.watchlistStocks[4].high} </td>
+                                    </tr>
+                                </tbody>
+                            </Table>
+                        </Card>                      
+                    </div>
+                    <Card color={`secondary`} inverse className="newcard">Insert chart here
+                    <img className="img1" src="https://media.ycharts.com/charts/c511f80ec356858029d034295d969d3d.png" title="Title of image" alt="alt text here"/>
+
+                    </Card>
+                </div>
+            </div>
+
+
+        );
+
+
+    }
+
+}
---
+++ ClientApp/src/components/signup/signup.js
@@ -0,0 +1,121 @@
+import React, {Component} from "react";
+import {Alert, Button, Card, CardTitle, Input} from "reactstrap";
+import "./signup.css";
+
+export class Signup extends Component {
+    static displayName = Signup.name;
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            loggedin: false,
+            usernameInput: "",
+            passwordInput: "",
+            firstnameInput: "",
+            lastnameInput: "",
+            emailInput: "",
+            signupResponse: [],
+            
+        };
+    }
+
+    usernameInput(e) {
+        this.setState({
+            usernameInput: e.target.value,
+        })
+    }
+
+    passwordInput(e) {
+        this.setState({
+                passwordInput: e.target.value,
+            }
+        )
+    }
+    firstnameInput(e) {
+        this.setState({
+                firstnameInput: e.target.value,
+            }
+        )
+    }
+    lastnameInput(e) {
+        this.setState({
+                lastnameInput: e.target.value,
+            }
+        )
+    }
+    emailInput(e) {
+        this.setState({
+                emailInput: e.target.value,
+            }
+        )
+    }
+
+
+    async signupButtonPress(){
+        const requestOptions = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                name: this.state.usernameInput,
+                alias: this.state.passwordInput,
+            })
+        }
+        let response = await fetch('api/persons/',requestOptions)
+      {         
+             let alerts = this.state.signupResponse
+            alerts.push({
+                title: "Account Created",
+            description: "Please login",
+                success: true
+            })
+            this.setState({signupResponse: alerts});
+        }
+    }
+    
+    
+    generateAlerts(alerts) {
+        return (
+            <div>
+                {alerts.map((alert, index) =>
+                    <Alert color={alert.success ? 'success' : 'danger'} toggle={() => this.removeAlert(index)}>
+                        <h5>{alert.title}</h5>
+                        <p>{alert.description}</p>
+                    </Alert>
+                )}
+            </div>
+        );
+    }
+
+    render () {
+        return (
+            <div className="outerContainer3">
+                <div className="innerContainer3">
+                    <div className="loginContainer3">
+                        <Card color={`secondary`} inverse className="loginCard3 innerContainerItem3">
+                            <CardTitle>
+                                <b>Enter account information</b>
+                            </CardTitle>
+                            <div className="loginField3">
+                                Username:
+                                <Input placeholder={`Create a username`} onChange={this.usernameInput.bind(this)}
+                                       value={this.state.userNameInput}/>
+                            </div>
+                            
+                            <div className="loginField3">
+                                Password:
+                                <Input type="password" placeholder={`Create a password`} onChange={this.passwordInput.bind(this)}
+                                       value={this.state.passwordInput}/>
+                            </div>
+                        </Card>
+                        
+                        <div className="innerContainerItem3">
+                            <Button color="light" className="signupButton" outline onClick={this.signupButtonPress.bind(this)} href={`/login`}>
+                                Create Account!
+                            </Button>
+                        </div>
+                    </div>
+                </div> 
+            </div>      
+        );
+    }
+}
